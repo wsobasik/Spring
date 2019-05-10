a3c4da777e6d60508255f10df0f17a1608e692a3
wsobasik <wojciech.sobasik@gmail.com>
2019-05-10 14:46:48 +0200

    Age parameter that is optional added

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+
+
+    @GetMapping("/hello")
+    private String sayHello(String userName, String userAge, Model model) {
+        model.addAttribute("userName", userName);
+        model.addAttribute("userAge", userAge);
+        return "hello";
+    }
 }
